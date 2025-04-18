2036dde6c60508f0cf88a5f8d80f2c2e458d1a75
James <blutegel@users.noreply.github.com>
2025-04-18 08:06:50 -0700

    Revision 0.9.5

--- src/main/java/bank/geld/BankApplication.java
+++ src/main/java/bank/geld/BankApplication.java
@@ -4,7 +4,6 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.scene.paint.Color;
 import java.io.IOException;
 
 public class BankApplication extends Application {
